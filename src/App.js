c009a79b09a282d0cadf5f148e691b1be5c3d665
Caleb Taylor <aquaductape@gmail.com>
2020-06-08 18:37:59 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,113 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import data from "./data";
+import Section from "./Section";
+
+const multiplyList = (arr, amount) => {
+  const newArr = [];
+  for (let i = 0; i < amount; i++) {
+    newArr.push(...arr);
+  }
+  return newArr;
+};
 
 function App() {
+  const [list, setList] = useState(
+    multiplyList(data, 3).map((item, idx) => ({ id: idx, item }))
+  );
+  const [isFinished, setIsFinished] = useState(false);
+  const [isRemoved, setIsRemoved] = useState(Date.now());
+  const sentinelCollectionRef = useRef(new Map());
+  const observerRef = useRef(
+    new IntersectionObserver(
+      (entries) => {
+        const sentinelContainer = sentinelCollectionRef.current;
+
+        entries.forEach((entry) => {
+          const { target, intersectionRatio, boundingClientRect } = entry;
+          const targetTop = boundingClientRect.top;
+          const rootHeight = entry.rootBounds.height;
+          const rootTop = entry.rootBounds.top;
+          const props = sentinelContainer.get(target);
+          const percentage = rootHeight > 700 ? 0.65 : 0.8;
+
+          if (!props) return;
+
+          const { container, setSticky, setShadow } = props;
+
+          const containerHeight = container.getBoundingClientRect().height;
+
+          if (containerHeight > rootHeight * percentage) {
+            setSticky(true);
+            const nearTop = Math.abs(targetTop - rootTop) <= rootHeight;
+            if (
+              intersectionRatio === 0 &&
+              (nearTop || Math.sign(targetTop) === -1)
+            ) {
+              setShadow(true);
+            } else if (intersectionRatio === 1) {
+              setShadow(false);
+            }
+          } else {
+            setSticky(false);
+            setShadow(false);
+          }
+        });
+      },
+      { threshold: [0, 1] }
+    )
+  );
+
+  useEffect(() => {
+    setIsFinished(true);
+  }, [isFinished]);
+
+  // cleanup
+  useEffect(() => {
+    const observer = observerRef.current;
+    const sentinelCollection = sentinelCollectionRef.current;
+
+    return () => {
+      observer.disconnect();
+      observer = null;
+      sentinelCollection.clear();
+    };
+  }, []);
+
+  const onRemove = (id) => {
+    const copy = [...list];
+    const foundIdx = list.findIndex((item) => item.id === id);
+    copy.splice(foundIdx, 1);
+    setList(copy);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sections with IntersectionObserver </h1>
+      <div>
+        If a section takes more 65% of the viewport, then the header title
+        floats
+      </div>
+      <br />
+      <div>If viewport height is less than 700px, it's 80%</div>
+      <br />
+      <div>
+        One Observer watching {sentinelCollectionRef.current.size}{" "}
+        {sentinelCollectionRef.current.size - 1 ? "sections" : "section"}
+      </div>
+
+      {list.map(({ id, item }) => {
+        return (
+          <Section
+            id={id}
+            content={item}
+            observer={observerRef.current}
+            sentinelCollection={sentinelCollectionRef.current}
+            onRemove={onRemove}
+            setIsRemoved={setIsRemoved}
+            key={id}
+          ></Section>
+        );
+      })}
     </div>
   );
 }
---
+++ src/Section.js
@@ -0,0 +1,65 @@
+import React, { useState, useRef, useEffect } from "react";
+
+const Section = ({
+  id,
+  content,
+  observer,
+  sentinelCollection,
+  onRemove,
+  setIsRemoved,
+}) => {
+  const containerRef = useRef(null);
+  const sentinelTopRef = useRef(null);
+  const [sticky, setSticky] = useState(false);
+  const [shadow, setShadow] = useState(false);
+
+  useEffect(() => {
+    const sentinelTop = sentinelTopRef.current;
+    const container = containerRef.current;
+
+    observer.observe(sentinelTop);
+    sentinelCollection.set(sentinelTop, {
+      container,
+      setSticky,
+      setShadow,
+    });
+
+    // when component is removed
+    return () => {
+      console.log("on removed");
+      observer.unobserve(sentinelTop);
+      sentinelCollection.delete(sentinelTop, {
+        container,
+        setSticky,
+        setShadow,
+      });
+      setIsRemoved(Date.now());
+    };
+  }, []);
+
+  const styleHeader = {
+    position: sticky ? "sticky" : "relative",
+    // Shadow: casted only bottom, not on all sides
+    //      best style if header layout has no left padding
+    //      issue: may look strange
+    boxShadow: shadow ? "rgba(0, 0, 42, 0.55) 0px 15px 17px -16px" : "none",
+    // Shadow: normaly casted, all 4 or 3 sides
+    // best style if header layout is static
+    // paddingLeft: shadow ? "30px": "0",
+  };
+
+  return (
+    <section ref={containerRef}>
+      <div className="sentinel-top" ref={sentinelTopRef}></div>
+      <header style={styleHeader}>
+        <h3>Header {id + 1}</h3>{" "}
+        <button className="btn-remove" onClick={() => onRemove(id)}>
+          Remove
+        </button>
+      </header>
+      <div className="content">{content}</div>
+    </section>
+  );
+};
+
+export default Section;
