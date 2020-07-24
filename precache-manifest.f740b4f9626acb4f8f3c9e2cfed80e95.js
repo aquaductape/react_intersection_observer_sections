c4910fabd30fa63d7db86b3ad6fe2155b08c5bfe
Caleb Taylor <aquaductape@gmail.com>
2020-07-23 18:18:52 -0700

    Updates

---
+++ precache-manifest.f740b4f9626acb4f8f3c9e2cfed80e95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b853fd3914a43d03ff3e05834eca2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36911a435a0e5861be23",
+    "url": "/static/css/main.75549139.chunk.css"
+  },
+  {
+    "revision": "000b5721975b7fd959a3",
+    "url": "/static/js/2.bacbbe69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bacbbe69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36911a435a0e5861be23",
+    "url": "/static/js/main.41c6cc05.chunk.js"
+  },
+  {
+    "revision": "cb70b666e7d1fdcac157",
+    "url": "/static/js/runtime-main.f66ff79a.js"
+  }
+]);
